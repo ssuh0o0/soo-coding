2a277824567f9608518da526f2d1ee223ded6cf2
ssuh0o0 <ssuh0o0@daum.net>
2020-02-07 01:50:10 +0900

    큐,덱 1번문제

--- 큐,덱_1.cpp
+++ 큐,덱_1.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include<cstring>
+
 using namespace std;
 
 int main() {
+	cin.tie(0);
+	cin.sync_with_stdio(false);
 	int N;
 	cin >> N;
 	queue<int> q;
@@ -11,24 +15,37 @@ int main() {
 	char *ord=new char[20];
 	for (int i = 0; i < N; i++) {
 		cin >> ord;
-		if (strcmp(ord, "push")) {
+		if (!strcmp(ord,"push")) {
 			cin >> num;
 			q.push(num);
 		}
-		else if (strcmp(ord, "pop")){
+		else if (!strcmp(ord, "pop")){
 			if (q.empty()) cout << "-1";
-			q.pop();
+			else {
+				cout << q.front() << "\n";
+				q.pop();	
+				if (q.empty()) cout << "-1";
+			}
 		}
-		else if (strcmp(ord, "size")) {
-
+		else if (!strcmp(ord, "size")) {
+			int size = q.size();
+			cout << size << "\n";
 		}
-		else if (strcmp(ord, "empty")) {
+		else if (!strcmp(ord, "empty")) {
+			if (q.empty()) cout << "1" << "\n";
+			else cout << "0" << "\n";
 		}
-		else if (strcmp(ord, "front")) {
-
+		else if (!strcmp(ord, "front")) {
+			if (!q.empty()) {
+				cout << q.front() << "\n";
+			}
+			else cout << "-1" << "\n";
 		}
-		else if (strcmp(ord, "back")) {
-
+		else if (!strcmp(ord, "back")) {
+			if (!q.empty()) {
+				cout << q.back() << "\n";
+			}
+			else cout << "-1" << "\n";
 		}
 	}
 }
