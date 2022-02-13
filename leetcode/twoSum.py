6ed59ab7aee07e1d6f6d74fea491560f2657a4d5
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-02-13 23:21:14 +0900

    Update twoSum.py

--- leetcode/twoSum.py
+++ leetcode/twoSum.py
@@ -4,7 +4,4 @@ class Solution:
              for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
-                
-                
-![image](https://user-images.githubusercontent.com/55631147/153757387-72c067fd-bb6d-477c-80ef-78ca9841c875.png)
                   
