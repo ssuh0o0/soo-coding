08e1d8ac77915a4c8ad118472ffb23021bc05a2d
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-02-19 19:30:31 +0900

    Update plusOne.py

--- leetcode/plusOne.py
+++ leetcode/plusOne.py
@@ -3,7 +3,7 @@ class Solution:
         num = int("".join(map(str, digits)))
         return list(str(num+1))
     
-    
+# much faster code
 # class Solution:
 #     def plusOne(self, digits: List[int]) -> List[int]:
 #         num = "".join(map(str, digits))
