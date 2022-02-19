082447cddcafdd98639daec78ebe0b1b827eb222
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-02-19 19:29:54 +0900

    Update plusOne.py

--- leetcode/plusOne.py
+++ leetcode/plusOne.py
@@ -1,4 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = int("".join(List))
+        num = int("".join(map(str, digits)))
         return list(str(num+1))
+    
+    
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         num = "".join(map(str, digits))
+#         num = int(num) + 1
+#         num = str(num)
+#         return list(num)
