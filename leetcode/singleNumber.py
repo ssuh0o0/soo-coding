20a1ae486402a42937cdf25694e1d6ed835d9d51
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-03-01 00:27:55 +0900

    Update singleNumber.py

--- leetcode/singleNumber.py
+++ leetcode/singleNumber.py
@@ -1,4 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
         nums = sorted(nums)
-        for i in range(len(nums-1)):
+        for i in range(0,len(nums)-1,2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+            
+        return nums[-1]
+    
+    
+# 대박 아이디어
+# 가능한 이유:  같은 숫자 ^ 같은 숫자 = 0 이 나오기때문.. 
+#  class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+#         result = 0
+#
+#         for num in nums:
+#             result = result ^ num
+#        
+#         return result
