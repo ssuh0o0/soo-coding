5aca1d8f034df0cf0bb676366e08b888dc307073
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2022-04-01 23:27:39 +0900

    Update rearrangeProductsTable.sql

--- DBleetCode/rearrangeProductsTable.sql
+++ DBleetCode/rearrangeProductsTable.sql
@@ -1,3 +1,5 @@
+/* using pivot and not clear because of NULL Checking */
+
 SELECT product_id,
         CASE WHEN x=1 THEN 'store1'   
              WHEN x=2 THEN 'store2'
@@ -18,4 +20,21 @@ FROM (
 ORDER BY product_id, store
 
 
-/* using pivot */
+
+/* using only union and clear answer */
+
+SELECT product_id, 'store1' as store, store1 as price from Products
+WHERE store1 IS NOT NULL
+
+UNION
+
+SELECT product_id, 'store2' as store, store2 as price from Products
+WHERE store2 IS NOT NULL
+
+UNION
+
+SELECT product_id, 'store3' as store, store3 as price from Products
+WHERE store3 IS NOT NULL
+
+
+
